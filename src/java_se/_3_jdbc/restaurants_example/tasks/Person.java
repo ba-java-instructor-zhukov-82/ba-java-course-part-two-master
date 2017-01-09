6901047c99537bff50ab34475651a9a669c80b43
ba-java-instructor-zhukov-82 <ba.java.instructor.zhukov.82@gmail.com>
2017-01-09 21:47:41 +0200

    Person Persistence Classes added commit

---
+++ src/java_se/_3_jdbc/restaurants_example/tasks/Person.java
@@ -0,0 +1,34 @@
+package java_se._3_jdbc.restaurants_example.tasks;
+
+public class Person {
+
+    private String firstName;
+    private String LastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person() {}
+}
--- src/java_se/_3_jdbc/restaurants_example/tasks/PersonPersistence.java
+++ src/java_se/_3_jdbc/restaurants_example/tasks/PersonPersistence.java
@@ -1,31 +1,50 @@
-package java_se._3_jdbc.restaurants_example;
+package java_se._3_jdbc.restaurants_example.tasks;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSetMetaData;
+import java_se._3_jdbc.restaurants_example.DerbyHelper;
 
+import java.sql.*;
+import java.util.Scanner;
 
-public class Restaurants {
+public class PersonPersistence {
+
     private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-    private static final String DB_NAME = "restaurantsDB";
+    private static final String DB_NAME = "Persons Persistence DB";
     private static final String CREATE = "create=true";
     private static final String USER = "user=admin";
     private static final String PASSWORD = "password=root";
     private static final String DB_PROPERTIES = String.format("%s;%s;%s", CREATE, USER, PASSWORD);
     private static final String DB_URL = "jdbc:derby:" + DB_NAME + ";" + DB_PROPERTIES;
 
-    private static final String TABLE_NAME = "restaurants";
+    private static final String TABLE_NAME = "PERSONS";
     private static Connection conn = null;
     private static Statement stmt = null;
+
+    private static Person p = new Person();
+    private static Scanner input = new Scanner(System.in);
+    private static final int NUMBER_OF_PERSONS = 3;
 
     public static void main(String[] args) {
         createConnection();
         createTable();
-        //insertRestaurants("Hollywood", "Odesa");
-        selectRestaurants();
+
+        /*for(int i = 0; i < NUMBER_OF_PERSONS; ++i) {
+            System.out.println("Please, enter First Name:");
+            p.setFirstName(input.nextLine());
+            System.out.println("Please, enter Last Name:");
+            p.setLastName(input.nextLine());
+            System.out.println("Please, enter Age:");
+            p.setAge(Integer.parseInt(input.nextLine()));
+            System.out.println(new String(new char[20]).replace('\0', '-'));
+            insertPerson();
+        }*/
+
+        selectPersons();
+        deletePersons();
+        System.out.println("\n\n");
+        selectPersons();
+        updatePerson();
+        System.out.println("\n\n");
+        selectPersons();
         //DerbyHelper.printTables(conn, DB_URL, TABLE_NAME);
         shutdown();
     }
@@ -33,7 +52,6 @@ public class Restaurants {
     private static void createConnection() {
         try {
             Class.forName(DRIVER).newInstance();
-            //Get a connection
             conn = DriverManager.getConnection(DB_URL);
         } catch (Exception except) {
             except.printStackTrace();
@@ -47,8 +65,9 @@ public class Restaurants {
                 stmt.execute("CREATE TABLE " + TABLE_NAME + "\n" +
                         "(\n" +
                         "ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),\n" +
-                        "NAME varchar(128),\n" +
-                        "CITY varchar(128),\n" +
+                        "FIRST_NAME varchar(128),\n" +
+                        "LAST_NAME varchar(128),\n" +
+                        "AGE INT,\n" +
                         "CONSTRAINT primary_key PRIMARY KEY (ID)" +
                         ")");
             }
@@ -57,40 +76,64 @@ public class Restaurants {
         }
     }
 
-    private static void insertRestaurants(String restName, String cityName) {
+    private static void insertPerson() {
+
+
         try {
             stmt = conn.createStatement();
-            stmt.execute("insert into " + TABLE_NAME + "(NAME, CITY)" +
-                        " values ('" + restName + "','" + cityName + "')");
+            stmt.execute("insert into " + TABLE_NAME + "(FIRST_NAME, LAST_NAME, AGE)" +
+                    " values ('" + p.getFirstName() + "','" + p.getLastName() + "', " + p.getAge() + ")");
             stmt.close();
         } catch (SQLException sqlExcept) {
             sqlExcept.printStackTrace();
         }
     }
 
-    private static void selectRestaurants() {
+    private static void selectPersons() {
         try {
             stmt = conn.createStatement();
             ResultSet results = stmt.executeQuery("select * from " + TABLE_NAME);
             ResultSetMetaData rsmd = results.getMetaData();
             int numberCols = rsmd.getColumnCount();
-            for (int i = 1; i <= numberCols; i++) {
-                //print Column Names
+            for (int i = 1; i <= numberCols; i++)
                 System.out.print(rsmd.getColumnLabel(i) + "\t\t");
-            }
 
             System.out.println("\n-------------------------------------------------");
 
             while (results.next()) {
                 int id = results.getInt(1);
-                String restName = results.getString(2);
-                String cityName = results.getString(3);
-                System.out.println(id + "\t\t" + restName + "\t\t" + cityName);
+                String firstName = results.getString(2);
+                String lastName = results.getString(3);
+                int age = results.getInt(4);
+                System.out.println(id + "\t\t" + firstName + "\t\t" + lastName + "\t\t" + age);
             }
             results.close();
             stmt.close();
         } catch (SQLException sqlExcept) {
             sqlExcept.printStackTrace();
+        }
+    }
+
+    private static void updatePerson() {
+        try {
+            Thread.sleep(2000);
+            stmt = conn.createStatement();
+            stmt.execute("UPDATE " + TABLE_NAME + " SET FIRST_NAME = 'Olenka'" +
+                         "WHERE FIRST_NAME = 'Olena'");
+            stmt.close();
+        } catch (SQLException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deletePersons() {
+        try {
+            Thread.sleep(2000);
+            stmt = conn.createStatement();
+            stmt.execute("DELETE FROM " + TABLE_NAME + " WHERE FIRST_NAME = 'Mykola'");
+            stmt.close();
+        } catch (SQLException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
